85d445b8d64865bf6ff1baecfe275db816127c2d
vkavinmech <99529655+Jaey12@users.noreply.github.com>
2025-06-16 10:41:11 +0530

    test
    commit

--- tests/ExtraUIValidation.spec.js
+++ tests/ExtraUIValidation.spec.js
@@ -7,4 +7,18 @@ test.only('Calendar playwright Script',async({browser, page})=>
     await expect(page.locator(".inputs.displayed-class")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator(".inputs.displayed-class")).toBeHidden();
+
+    //Dialog Box
+    await page.locator("#confirmbtn").click();
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+    page.on('dialog', dialog => dialog.dismiss());
+
+    //Hover
+    await page.locator("#mousehover").hover();
+
+    //Frames
+    const framesPage = page.frameLocator("#courses-iframe");
+    await framesPage.locator(".login-btn").first().click();
+
 });
